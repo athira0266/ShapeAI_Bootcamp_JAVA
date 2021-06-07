4a04f91b5761dfc2124991c5e4b92c3962a8188d
athira0266 <84954853+athira0266@users.noreply.github.com>
2021-06-07 16:50:18 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <h1>Javascript and React.js<h1>
+        <p>this was an amazing bootcamp taken up by Shaurya sir.
+        </p>
+        </div>
+  );
+}
+
+export default Note;
+
